dedf5efc76c2ea70df844b176ea5f4f1006831ed
wuchao <1994644671@qq.com>
2022-01-30 11:21:35 +0800

    约束布局的探索

---
+++ app/src/main/java/com/example/androidLearn/L2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.androidLearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class L2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.l2_layout)
+    }
+}
---
+++ app/src/main/java/com/example/androidLearn/U1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.androidLearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class U1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.u1_layout)
+    }
+}
