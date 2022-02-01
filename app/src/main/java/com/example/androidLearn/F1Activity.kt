e472ff009475cbbecf21511fbeab3e2464a228ea
wuchao <1994644671@qq.com>
2022-02-01 22:43:42 +0800

    flagment修复

--- app/src/main/java/com/example/androidLearn/F1Activity.kt
+++ app/src/main/java/com/example/androidLearn/F1Activity.kt
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.androidLearn.databinding.F1LayoutBinding
-import com.example.androidLearn.databinding.LeftFragmentBinding
 
 private lateinit var binding: F1LayoutBinding
-private lateinit var bindingLeft: LeftFragmentBinding
 
 class F1Activity : AppCompatActivity() {
 
@@ -16,14 +14,9 @@ class F1Activity : AppCompatActivity() {
         binding = F1LayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(RightFragment())
-        bindingLeft = LeftFragmentBinding.inflate(layoutInflater)
-        bindingLeft.f1Btn1.setOnClickListener {
-            replaceFragment(AnotherRightFragment())
-            // 不执行
-        }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+    fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.rightFlagLayout, fragment)
--- app/src/main/java/com/example/androidLearn/LeftFragment.kt
+++ app/src/main/java/com/example/androidLearn/LeftFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class LeftFragment : Fragment() {
@@ -13,5 +14,15 @@ class LeftFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.left_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.f1Btn1).setOnClickListener {
+            if (activity != null) {
+                val f1 = activity as F1Activity
+                f1.replaceFragment(AnotherRightFragment())
+            }
+        }
     }
 }
