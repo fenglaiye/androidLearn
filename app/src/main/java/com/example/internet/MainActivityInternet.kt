d3e406a8a43f8b4b25cf5005fc8071bacae6e2e9
wuchao <1994644671@qq.com>
2022-02-07 22:29:35 +0800

    Pull解析xml

--- app/src/main/java/com/example/internet/MainActivityInternet.kt
+++ app/src/main/java/com/example/internet/MainActivityInternet.kt
@@ -2,12 +2,17 @@ package com.example.internet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebViewClient
+import com.example.androidLearn.Global
 import com.example.androidLearn.databinding.ActivityMainInternetBinding
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.StringReader
 import java.lang.Exception
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
@@ -31,12 +36,13 @@ class MainActivityInternet : AppCompatActivity() {
             try {
                 val client = OkHttpClient()
                 val request = Request.Builder()
-                    .url("https://www.baidu.com")
+                    .url("http://192.168.50.182/get_data.xml")
                     .build()
                 val response = client.newCall(request).execute()
                 val responseData = response.body?.string()
                 if (responseData != null) {
-                    showResponse(responseData)
+//                    showResponse(responseData)
+                    parseXML1(responseData)
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -48,6 +54,42 @@ class MainActivityInternet : AppCompatActivity() {
         runOnUiThread {
             // 在这里进行UI操作
             binding.responseText.text = response
+        }
+    }
+
+    private fun parseXML1(xmlData: String) {
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val xmlPullParser = factory.newPullParser()
+            xmlPullParser.setInput(StringReader(xmlData))
+            var eventType = xmlPullParser.eventType
+            var id = ""
+            var name = ""
+            var version = ""
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val nodeName = xmlPullParser.name
+                when (eventType) {
+                    // 开始解析某个节点
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    // 完成解析某个节点
+                    XmlPullParser.END_TAG -> {
+                        if ("app" == nodeName) {
+                            Log.d(Global.TAG, "id $id")
+                            Log.d(Global.TAG, "name is $name")
+                            Log.d(Global.TAG, "version is $version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
