99b89f5effbaec3e80f5fad7e12af50821848365
wuchao <1994644671@qq.com>
2022-02-08 13:34:25 +0800

    json解析的两种方式

--- app/src/main/java/com/example/internet/MainActivityInternet3.kt
+++ app/src/main/java/com/example/internet/MainActivityInternet3.kt
@@ -2,20 +2,18 @@ package com.example.internet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.androidLearn.Global
 import com.example.androidLearn.databinding.ActivityMainInternetBinding
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import org.json.JSONArray
+import org.xml.sax.InputSource
+import java.io.StringReader
 import java.lang.Exception
+import javax.xml.parsers.SAXParserFactory
 import kotlin.concurrent.thread
 
 private lateinit var binding: ActivityMainInternetBinding
 
-class MainActivityInternet : AppCompatActivity() {
+class MainActivityInternet3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainInternetBinding.inflate(layoutInflater)
@@ -30,12 +28,14 @@ class MainActivityInternet : AppCompatActivity() {
             try {
                 val client = OkHttpClient()
                 val request = Request.Builder()
-                    .url("http://192.168.3.247/get_data.json")
+                    .url("http://192.168.3.247/get_data.xml")
                     .build()
                 val response = client.newCall(request).execute()
                 val responseData = response.body?.string()
                 if (responseData != null) {
-                    parseJson2(responseData)
+//                    showResponse(responseData)
+//                    parseXML1(responseData)
+                    parseXML2(responseData)
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -50,29 +50,15 @@ class MainActivityInternet : AppCompatActivity() {
         }
     }
 
-    private fun parseJson1(jsonData: String) {
+    private fun parseXML2(xmlData: String) {
         try {
-            val jsonArray = JSONArray(jsonData)
-            for (i in 0 until jsonArray.length()) {
-                val jsonObject = jsonArray.getJSONObject(i)
-                val id = jsonObject.getString("id")
-                val name = jsonObject.getString("name")
-                val version = jsonObject.getString("version")
-                Log.d(Global.TAG, "$id $name $version")
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun parseJson2(jsonData: String) {
-        try {
-            val gson = Gson()
-            val typeOf = object : TypeToken<List<App>>() {}.type
-            val appList = gson.fromJson<List<App>>(jsonData, typeOf)
-            for (app in appList) {
-                Log.d(Global.TAG, "${app.id} ${app.name} ${app.version}")
-            }
+            val factory = SAXParserFactory.newInstance()
+            val xmlReader = factory.newSAXParser().xmlReader
+            val handler = ContentHandler()
+            //  将ContentHandler实例设置到reader中
+            xmlReader.contentHandler = handler
+            // 执行解析
+            xmlReader.parse(InputSource(StringReader(xmlData)))
         } catch (e: Exception) {
             e.printStackTrace()
         }
